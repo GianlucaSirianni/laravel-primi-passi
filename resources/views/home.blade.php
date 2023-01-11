583594654977e634f538ca433a07310c8dd41886
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-01-11 14:41:20 +0100

    last commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+
+    <h1>Ecco il mio menù</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/WorkWithUs">Work With Us</a></li>
+        <li><a href="/vabbhecisiamocapiti">A Fourth Title</a></li>
+    </ul>
+
+    <style>
+
+    h1{
+        color: red;
+    }
+
+    </style>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/WorkWithUs', function(){
+    return view('work');
+});
+
+Route::get('/vabbhecisiamocapiti', function(){
+    return view('vabbe');
+});
+
